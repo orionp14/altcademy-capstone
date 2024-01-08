a1b6a54228f576428d2bf1658758b5ee9f66ece3
Orion Piotrowski <opiotrow@asu.edu>
2024-01-08 15:58:54 -0700

    front-end

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -4,24 +4,52 @@ import ReactDOM from 'react-dom';
 import './home.scss';
 
 const Home = () => (
-  <section className="">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-lg-6 vh-100 bg-primary">
+<section className="">
+  <header>
+    <h2>Chatbot</h2>
+  </header>
+  <div className="container-fluid g-0">
+    <div className="row g-0">
+      <div className="col-lg-6 logo-panel d-flex flex-column align-items-center justify-content-center">
+        <div className='circle mb-3'>
+          <span className="material-symbols-outlined main-logo">smart_toy</span>
         </div>
-        <div className="col-lg-6 vh-100 chat-container">
-          <h1>ChatGPT</h1>
-          <section className="chat-conversation"></section>
-          <form className="chat-input">
-            <div className="input-group">
-              <textarea className="form-control" id="prompt" placeholder="Type your message..."></textarea>
-              <button type="submit" className="btn btn-primary">Send</button>
+        <p className="text-center mb-4 disclaimer">
+            <span className="disclaimer-title">Welcome to our AI Medical Chatbot!</span>
+            <br />
+            We're here to help you determine your illness and provide guidance on combating symptoms.
+            <br />
+            Please note that this is not a substitute for professional medical advice.
+          </p>
+        <div className="button-panel justify-content-center">
+          <button className="action-button btn btn-outline-primary w-100">Sickness</button>
+          <button className="action-button btn btn-outline-primary w-100">Injury</button>
+          <button className="action-button btn btn-outline-primary w-100">Allergies</button>
+          <button className="action-button btn btn-outline-primary w-100">Other</button>
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <div className="chatbot">
+          <div className="chatbot-content">
+            <ul className="chatbox">
+              <li className="chat incoming">
+                <span className="material-symbols-outlined">smart_toy</span>
+                <p>Hi there 👋<br/>How can I help you today?</p>
+              </li>
+              <li className="chat outgoing">
+                <p>I have a medical question. I am not feeling well.</p>
+              </li>
+            </ul>
+            <div className="chat-input">
+              <textarea placeholder="Enter a message..." spellCheck={false} required></textarea>
+              <span id="send-btn" className="material-symbols-rounded">send</span>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
 );
 
 document.addEventListener('DOMContentLoaded', () => {
